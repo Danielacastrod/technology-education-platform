0081c2c87f7b1f76294a5d54453a80e2321bf911
Dinaide Barbosa Da Silva <132600485+dinaideb@users.noreply.github.com>
2023-08-02 11:56:19 -0300

    Adicionei tela pós login e sala de aula

--- my-app/src/screen/Home.js
+++ my-app/src/screen/Home.js
@@ -22,7 +22,80 @@ export default function Home() {
 
   return (
     <div className="App">
-      <h1>Home</h1>
+      <h2>Sala de aula</h2>
+      <div className="saladeaula">
+        <button className="serie">
+        <a className="nomeSerie" href="file:///C:/Users/Rede2102/Desktop/sala%20de%20aula%20DevPro/index.html"> 3° ano</a></button>
+
+        <button className="serie">
+        <a className="nomeSerie" href="file:///C:/Users/Rede2102/Desktop/sala%20de%20aula%20DevPro/index.html"> 4° ano</a></button>
+
+        <button className="serie">
+        <a className="nomeSerie" href="file:///C:/Users/Rede2102/Desktop/sala%20de%20aula%20DevPro/index.html"> 5° ano</a></button> 
+
+        
+        <button className="serie">
+        <a className="nomeSerie" href="file:///C:/Users/Rede2102/Desktop/sala%20de%20aula%20DevPro/index.html"> 6° ano</a></button>
+
+        <button className="serie">
+        <a className="nomeSerie" href="file:///C:/Users/Rede2102/Desktop/sala%20de%20aula%20DevPro/index.html"> 7° ano</a></button>
+        
+        <button className="serie">
+        <a className="nomeSerie" href="file:///C:/Users/Rede2102/Desktop/sala%20de%20aula%20DevPro/index.html"> 8° ano</a></button>
+
+          <button className="serie">    
+        <a className="nomeSerie" href="file:///C:/Users/Rede2102/Desktop/sala%20de%20aula%20DevPro/index.html"> 9° ano</a></button>
+       
+     </div>
+
+        <h2>Comece por Aqui</h2>
+        <div className="jogo1">
+            
+            <div className="rolagem"><button className="seta direita">&lt;</button></div>
+
+              <button className="botaoJogo">
+              <img className="imgJog" src="img/image 19.png" alt="https://studio.code.org/s/express-2023/lessons/2/levels/1?redirect_warning=true"/>
+              <a className="linkJogo" href="https://studio.code.org/s/express-2023/lessons/2/levels/1?redirect_warning=true">Depurando no labirinto</a></button> {/*vai direto pra plataforma de jogo escolhido */}
+
+              <button className="botaoJogo">
+              <img className="imgJog" src="img/image 20 (1).png" alt="https://flukeout.github.io/"/>
+              <a className="linkJogo" href="https://flukeout.github.io/">CSS Dinner</a></button>  {/* vai direto pra plataforma de jogo escolhido */}
+
+              <button className="botaoJogo">
+              <img className="imgJog" src="img/image 51.png" alt="http://robozzle.com/js/index.aspx"/>
+              <a className="linkJogo" href="http://robozzle.com/js/index.aspx">RoboZZle</a></button> {/* vai direto pra plataforma de jogo escolhido */} 
+
+              <button className="botaoJogo">
+              <img className="imgJog" src="img/image 19 (1).png" alt="https://play.elevatorsaga.com/"/>
+              <a className="linkJogo" href="https://play.elevatorsaga.com/">Criando arte com código</a></button> {/*vai direto pra plataforma de jogo escolhido */} 
+
+             <div className="rolagem"><button className="seta esquerda">&gt;</button> </div>
+           </div>
+
+           <h2>Aprenda css jogando</h2>
+           <div className="jogo1">
+
+              <div className="rolagem"><button className="seta direita">&lt;</button></div>
+
+              <button className="botaoJogo">
+              <img className="imgJog" src="img/image 50.png" alt="https://studio.code.org/s/express-2023/lessons/1/levels/1?redirect_warning=true"/>
+              <a className="linkJogo" href="https://studio.code.org/s/express-2023/lessons/1/levels/1?redirect_warning=true">Programação com Angry Birds</a></button> {/*vai direto pra plataforma de jogo escolhido*/}  
+            
+              <button className="botaoJogo">
+              <img class="imgJog" src="img/image 52.png" alt="https://blockly.games/"/>
+              <a className="linkJogo" href="https://blockly.games/">Blockly Games</a></button> {/*vai direto pra plataforma de jogo escolhido*/}
+            
+              <button className="botaoJogo">
+              <img className="imgJog" src="img/image 19 (2).png " alt="https://flexboxfroggy.com/"/>
+              <a className="linkJogo" href="https://flexboxfroggy.com/">Flexbox Froggy</a></button> {/*vai direto pra plataforma de jogo escolhido*/} 
+
+              <button className="botaoJogo">
+              <img className="imgJog" src="img/image 53.png" alt="https://studio.code.org/s/coursec-2023/lessons/6/levels/1?redirect_warning=true"/>
+              <a className="linkJogo" href="https://studio.code.org/s/coursec-2023/lessons/6/levels/1?redirect_warning=true">Criando arte com código</a></button> {/*  vai direto pra plataforma de jogo escolhido*/} 
+            
+            <div className="rolagem"> <button className="seta esquerda">&gt;</button></div>
+           </div>
+          
 
       <button className="button" onClick={Perfil}>
         Perfil
